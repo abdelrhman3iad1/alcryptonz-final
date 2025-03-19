41d51037a56c2aad9da2fc36259fdfbe228068c5
RahmaQotb <146798814+RahmaQotb@users.noreply.github.com>
2025-03-19 21:41:16 +0200

    cat-related localization

--- resources/views/Web/categories-related.blade.php
+++ resources/views/Web/categories-related.blade.php
@@ -25,41 +25,79 @@
                 <div class="col-md-8">
                     <div class="parent-news">
 
+                        @if (config('app.locale') == 'ar')
 
-                        @if (isset($categoryPosts))
-                            <!--post-->
-                            @foreach ($categoryPosts as $post)
-                                <a style="direction:rtl;overflow:hidden"
-                                    href="post.php?idPost={{ $post->id }}"target='_blank'>
-                                    <div class="small-post">
-                                        <div class="img-div"> <img src="{{ asset($post->image) }}" alt="image here">
-                                        </div>
-                                        <h3 style="color:black;word-wrap: break-word;">{{ $post->title_ar }}</h3>
-                                        <span> {{ $post->user->name }} &nbsp;<i class="fas fa-user"></i></span><br>
-                                        <span>{{ $post->created_at->format('Y-m-d') }}&nbsp; <i
-                                                class="far fa-calendar-alt"></i></span>&nbsp;&nbsp;
+                            @if (isset($categoryPosts))
+                                <!--post-->
+                                @foreach ($categoryPosts as $post)
+                                    <a style="direction:rtl;overflow:hidden"
+                                        href="{{ route('showPost', $post->id) }}" target="_blank">
+                                        <div class="small-post">
+                                            <div class="img-div"> <img src="{{ asset($post->image) }}" alt="image here">
+                                            </div>
+                                            <h3 style="color:black;word-wrap: break-word;">{{ $post->title_ar }}</h3>
+                                            <span> {{ $post->user->name }} &nbsp;<i class="fas fa-user"></i></span><br>
+                                            <span>{{ $post->created_at->format('Y-m-d') }}&nbsp; <i
+                                                    class="far fa-calendar-alt"></i></span>&nbsp;&nbsp;
 
-                                        @if ($post->category->name == 'مقالات شركاء')
-                                            <span> {{ $post->partner->name }} &nbsp; <i class="fas fa-tags"></i> </span>
-                                        @else
-                                            <span> {{ $post->category->name }} &nbsp; <i class="fas fa-tags"></i>
-                                            </span>
-                                        @endif
-
-                                        <p style="color:black;">
-                                            @if (strlen($post->content_ar) > 150)
-                                                {{ strip_tags(substr(str_replace('&nbsp;', ' ', $post->content_ar), 0, 350) . '....') }}
+                                            @if ($post->category->name == 'مقالات شركاء')
+                                                <span> {{ $post->partner->name }} &nbsp; <i class="fas fa-tags"></i>
+                                                </span>
                                             @else
-                                                {{ strip_tags(str_replace('&nbsp;', ' ', $post->content_ar)) }}
+                                                <span> {{ $post->category->name }} &nbsp; <i class="fas fa-tags"></i>
+                                                </span>
                                             @endif
-                                        </p>
-                                    </div>
-                                </a>
-                            @endforeach
+
+                                            <p style="color:black;">
+                                                @if (strlen($post->content_ar) > 150)
+                                                    {{ strip_tags(substr(str_replace('&nbsp;', ' ', $post->content_ar), 0, 350) . '....') }}
+                                                @else
+                                                    {{ strip_tags(str_replace('&nbsp;', ' ', $post->content_ar)) }}
+                                                @endif
+                                            </p>
+                                        </div>
+                                    </a>
+                                @endforeach
+                            @else
+                                <b>
+                                    <center>{{ __('translation.no posts') }} </center>
+                                </b>
+                            @endif
+
                         @else
-                            <b>
-                                <center>لا يوجد منشورات </center>
-                            </b>
+                            @if (isset($categoryPosts))
+                                @foreach ($categoryPosts as $post)
+                                    <a style="direction:ltr;overflow:hidden" href="{{ route('showPost', $post->id) }}" target="_blank">
+                                        <div class="small-post">
+                                            <div class="img-div"> <img src="{{ asset($post->image) }}" alt="image here">
+                                            </div>
+                                            <h3 style="color:black;word-wrap: break-word;">{{ $post->title_en }}</h3>
+                                            <span> {{ $post->user->name }} &nbsp;<i class="fas fa-user"></i></span><br>
+                                            <span>{{ $post->created_at->format('Y-m-d') }}&nbsp; <i
+                                                    class="far fa-calendar-alt"></i></span>&nbsp;&nbsp;
+
+                                            @if ($post->category->name == 'مقالات شركاء')
+                                                <span> {{ $post->partner->name }} &nbsp; <i class="fas fa-tags"></i> </span>
+                                            @else
+                                                <span> {{ $post->category->name }} &nbsp; <i class="fas fa-tags"></i>
+                                                </span>
+                                            @endif
+
+                                            <p style="color:black;">
+                                                @if (strlen($post->content_en) > 150)
+                                                    {{ strip_tags(substr(str_replace('&nbsp;', ' ', $post->content_en), 0, 350) . '....') }}
+                                                @else
+                                                    {{ strip_tags(str_replace('&nbsp;', ' ', $post->content_en)) }}
+                                                @endif
+                                            </p>
+                                        </div>
+                                    </a>
+                                @endforeach
+                            @else
+                                <b>
+                                    <center>{{ __('translation.no posts') }} </center>
+                                </b>
+                            @endif
                         @endif
 
                     </div>
@@ -67,32 +105,22 @@
 
                 <div class="col-md-4">
                     <!-- start social  -->
-                    <?php /* include_once 'include/social.php'; */  ?>
-                    @include("Web.include.social")
+                    @include('Web.include.social')
                     <!-- end social  -->
                     <!-- start latest Partners posts -->
-                    <?php /* include_once 'include/lateast-Partners-Posts.php'; */  ?>
-                    @include("Web.include.lateast-Partners-Posts")
+                    @include('Web.include.lateast-Partners-Posts')
                     <!-- end latest Partners posts -->
                     <!-- start latest posts -->
-                    @include("Web.include.lateastPosts")
-
-                    <?php /* include_once 'include/lateastPosts.php'; */ ?>
+                    @include('Web.include.lateastPosts')
                     <!-- end latest posts -->
                     <!-- start latest alcrypto posts -->
-                    @include("Web.include.newsAl")
-
-                    <?php /* include_once 'include/newsAl.php'; */  ?>
+                    @include('Web.include.newsAl')
                     <!-- end latest alcrypto posts -->
                     <!-- start  categoreis -->
-                    @include("Web.include.catPart")
-
-                    <?php /* include_once 'include/catPart.php'; */  ?>
+                    @include('Web.include.catPart')
                     <!-- end categoreis -->
                     <!-- start contr  -->
-                    @include("Web.include.contr")
-
-                    <?php /* include_once 'include/contr.php'; */ ?>
+                    @include('Web.include.contr')
                     <!-- end contr  -->
                 </div>
             </div>
@@ -104,14 +132,14 @@
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-YN940EKWRS"></script>
 
-
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2473974671507788"
         crossorigin="anonymous"></script>
 
-
+    <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
     gtag('config', 'G-YN940EKWRS');
     </script>
+</body>     
