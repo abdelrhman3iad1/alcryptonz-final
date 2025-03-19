bae4b166684622d4af330ad142536ab3f411a1d4
abdelrhman3iad1 <abdelrhman3iiaad27@gmail.com>
2025-03-19 19:14:41 +0200

    privacy policy local

--- lang/ar/translation.php
+++ lang/ar/translation.php
@@ -25,7 +25,89 @@ return
     "CONTACT_US" => "اتصل بنا",
     "Keywords"=>"كلمات دلالية",
     "No Exisiting Categories"=>"لا يوجد تصنيفات",
+    "At Alcryptonz, accessible from alcryptonz.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Alcryptonz and how we use it."=>"في Alcryptonz، الذي يمكن الوصول إليه من alcryptonz.com، تتمثل إحدى أولوياتنا الرئيسية في خصوصية زوارنا. تحتوي وثيقة سياسة الخصوصية هذه على أنواع من المعلومات التي تم جمعها وتسجيلها بواسطة Alcryptonz وكيفية استخدامها.",
     "Contact Info"=>"معلومات التواصل",
+    "If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us."=>"إذا كانت لديك أسئلة إضافية أو طلبت مزيدًا من المعلومات حول سياسة الخصوصية الخاصة بنا، فلا تتردد في الاتصال بنا.",
+    "This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Alcryptonz. This policy is not applicable to any information collected offline or via channels other than this website."=>"تنطبق سياسة الخصوصية هذه فقط على أنشطتنا عبر الإنترنت وهي صالحة لزوار موقع الويب الخاص بنا فيما يتعلق بالمعلومات التي شاركوها و/أو جمعوها في Alcryptonz. لا تنطبق هذه السياسة على أي معلومات تم جمعها دون اتصال بالإنترنت أو عبر قنوات أخرى غير هذا الموقع.",
+    "Consent"=>"موافقة",
+    "By using our website, you hereby consent to our Privacy Policy and agree to its terms."=>"باستخدام موقع الويب الخاص بنا، توافق بموجب هذا على سياسة الخصوصية الخاصة بنا وتوافق على شروطها.",
+    "Information we collect"=>"المعلومات التي نجمعها",
+    'The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.' => 'سيتم توضيح المعلومات الشخصية التي يُطلب منك تقديمها، وأسباب طلبها، في النقطة التي نطلب منك فيها تقديم معلوماتك الشخصية.',
+    
+    'If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.' => 'إذا اتصلت بنا مباشرة، فقد نتلقى معلومات إضافية عنك مثل اسمك وعنوان بريدك الإلكتروني ورقم هاتفك ومحتويات الرسالة و/أو المرفقات التي قد ترسلها إلينا، وأي معلومات أخرى قد تختار تقديمها.',
+    
+    'When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.' => 'عند التسجيل للحصول على حساب، قد نطلب معلومات الاتصال الخاصة بك، بما في ذلك عناصر مثل الاسم واسم الشركة والعنوان وعنوان البريد الإلكتروني ورقم الهاتف.',
+    
+    'How we use your information' => 'كيف نستخدم معلوماتك',
+    
+    'We use the information we collect in various ways, including to:' => 'نستخدم المعلومات التي نجمعها بطرق مختلفة، بما في ذلك:',
+    
+    'Provide, operate, and maintain our website' => 'توفير وتشغيل وصيانة موقعنا الإلكتروني',
+    
+    'Improve, personalize, and expand our website' => 'تحسين وتخصيص وتوسيع موقعنا الإلكتروني',
+    
+    'Understand and analyze how you use our website' => 'فهم وتحليل كيفية استخدامك لموقعنا',
+    
+    'Develop new products, services, features, and functionality' => 'تطوير منتجات وخدمات وميزات ووظائف جديدة',
+    
+    'Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes' => 'التواصل معك، إما مباشرة أو من خلال أحد شركائنا، بما في ذلك خدمة العملاء، لتزويدك بالتحديثات وغيرها من المعلومات المتعلقة بالموقع، ولأغراض التسويق والترويج',
+    
+    'Send you emails' => 'إرسال رسائل البريد الإلكتروني إليك',
+    
+    'Find and prevent fraud' => 'اكتشاف ومنع الاحتيال',
+    
+    'Log Files' => 'ملفات السجل',
+    
+    "Alcryptonz follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information." => "تتبع Alcryptonz إجراءً قياسيًا لاستخدام ملفات السجل. تقوم هذه الملفات بتسجيل الزوار عند زيارتهم للمواقع الإلكترونية. تقوم جميع شركات الاستضافة بذلك وهو جزء من تحليلات خدمات الاستضافة. تشمل المعلومات التي تجمعها ملفات السجل عناوين بروتوكول الإنترنت (IP)، ونوع المتصفح، ومزود خدمة الإنترنت (ISP)، وطابع التاريخ والوقت، وصفحات الإحالة/الخروج، وربما عدد النقرات. هذه ليست مرتبطة بأي معلومات يمكن التعرف عليها شخصيًا. الغرض من المعلومات هو تحليل الاتجاهات، وإدارة الموقع، وتتبع حركة المستخدمين على الموقع، وجمع المعلومات الديموغرافية.",
+    
+    'Advertising Partners Privacy Policies' => 'سياسات الخصوصية لشركاء الإعلان',
+    
+    'You may consult this list to find the Privacy Policy for each of the advertising partners of Alcryptonz.' => 'يمكنك الرجوع إلى هذه القائمة للعثور على سياسة الخصوصية لكل شريك من شركاء الإعلان في Alcryptonz.',
+    
+    "Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Alcryptonz, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit." => "تستخدم خوادم الإعلانات التابعة لجهات خارجية أو شبكات الإعلانات تقنيات مثل ملفات تعريف الارتباط أو JavaScript أو إشارات الويب المستخدمة في إعلاناتها وروابطها التي تظهر على Alcryptonz، والتي يتم إرسالها مباشرة إلى متصفح المستخدمين. يتلقون تلقائيًا عنوان IP الخاص بك عند حدوث ذلك. تُستخدم هذه التقنيات لقياس فعالية حملاتهم الإعلانية و/أو لتخصيص المحتوى الإعلاني الذي تراه على المواقع التي تزورها.",
+    
+    'Note that Alcryptonz has no access to or control over these cookies that are used by third-party advertisers.' => 'لاحظ أن Alcryptonz ليس لديها حق الوصول إلى أو التحكم في ملفات تعريف الارتباط هذه التي تستخدمها الجهات الإعلانية الخارجية.',
+    
+    'Third Party Privacy Policies' => 'سياسات خصوصية الطرف الثالث',
+    
+    "Alcryptonz's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options." => "لا تنطبق سياسة خصوصية Alcryptonz على المعلنين الآخرين أو المواقع الإلكترونية. وبالتالي، فإننا ننصحك بالرجوع إلى سياسات الخصوصية الخاصة بخوادم الإعلانات التابعة للجهات الخارجية هذه للحصول على معلومات أكثر تفصيلاً. قد تتضمن ممارساتها وتعليماتها حول كيفية إلغاء الاشتراك في خيارات معينة.",
+    
+    "You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites." => "يمكنك اختيار تعطيل ملفات تعريف الارتباط من خلال خيارات المتصفح الفردية الخاصة بك. لمعرفة معلومات أكثر تفصيلاً حول إدارة ملفات تعريف الارتباط مع متصفحات الويب المحددة، يمكن العثور عليها في مواقع المتصفحات المعنية.",
+    
+    'CCPA Privacy Rights (Do Not Sell My Personal Information)' => 'حقوق خصوصية CCPA (لا تبيع معلوماتي الشخصية)',
+    "Privacy Policy for AlCryptonz"=>"سياسات الخصوصية للكريبتونز",
+    
+    'Under the CCPA, among other rights, California consumers have the right to:' => 'بموجب CCPA، من بين حقوق أخرى، يتمتع مستهلكو كاليفورنيا بالحق في:',
+    
+    "Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers." => "طلب أن تكشف الشركة التي تجمع البيانات الشخصية للمستهلك عن فئات وقطع محددة من البيانات الشخصية التي جمعتها الشركة عن المستهلكين.",
+    
+    'Request that a business delete any personal data about the consumer that a business has collected.' => 'طلب أن تحذف الشركة أي بيانات شخصية عن المستهلك جمعتها الشركة.',
+    
+    "Request that a business that sells a consumer's personal data, not sell the consumer's personal data." => "طلب ألا تبيع الشركة التي تبيع البيانات الشخصية للمستهلك بيانات المستهلك الشخصية.",
+    
+    'If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.' => 'إذا قدمت طلبًا، فلدينا شهر واحد للرد عليك. إذا كنت ترغب في ممارسة أي من هذه الحقوق، يرجى الاتصال بنا.',
+    
+    'GDPR Data Protection Rights' => 'حقوق حماية البيانات وفقًا للائحة العامة لحماية البيانات (GDPR)',
+    
+    'We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:' => 'نود التأكد من أنك على دراية كاملة بجميع حقوق حماية البيانات الخاصة بك. يحق لكل مستخدم ما يلي:',
+    
+    'The right to access – You have the right to request copies of your personal data. We may charge you a small fee for this service.' => 'الحق في الوصول - لديك الحق في طلب نسخ من بياناتك الشخصية. قد نفرض عليك رسومًا صغيرة مقابل هذه الخدمة.',
+    
+    'The right to rectification – You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.' => 'الحق في التصحيح - لديك الحق في طلب تصحيح أي معلومات تعتقد أنها غير دقيقة. لديك أيضًا الحق في طلب استكمال المعلومات التي تعتقد أنها غير كاملة.',
+    
+    'The right to erasure – You have the right to request that we erase your personal data, under certain conditions.' => 'الحق في المحو - لديك الحق في طلب محو بياناتك الشخصية، في ظل ظروف معينة.',
+    
+    'The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.' => 'الحق في تقييد المعالجة - لديك الحق في طلب تقييد معالجة بياناتك الشخصية، في ظل ظروف معينة.',
+    
+    'The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions.' => 'الحق في الاعتراض على المعالجة - لديك الحق في الاعتراض على معالجتنا لبياناتك الشخصية، في ظل ظروف معينة.',
+    
+    'The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.' => 'الحق في نقل البيانات - لديك الحق في طلب نقل البيانات التي جمعناها إلى منظمة أخرى، أو مباشرة إليك، في ظل ظروف معينة.',
+    
+    "Children's Information" => "معلومات الأطفال",
+    
+    'Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.' => 'جزء آخر من أولويتنا هو إضافة حماية للأطفال أثناء استخدام الإنترنت. نشجع الآباء والأوصياء على مراقبة والمشاركة في و/أو مراقبة وتوجيه نشاطهم عبر الإنترنت.',
+    
+    'Alcryptonz does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.' => 'لا تجمع Alcryptonz عن علم أي معلومات شخصية قابلة للتعريف من الأطفال دون سن 13 عامًا. إذا كنت تعتقد أن طفلك قدم هذا النوع من المعلومات على موقعنا، فإننا نشجعك بشدة على الاتصال بنا على الفور وسنبذل قصارى جهدنا لإزالة هذه المعلومات من سجلاتنا على الفور.',
     "Community_Management" => "إدارة المجتمع",
     "Experts in community management and currently working with several Crypto projects" => "خبراء في إدارة المجتمع ويعملون حاليًا مع عدة مشاريع في مجال العملات الرقمية",
     "Marketing" => "التسويق",
